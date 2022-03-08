ed4d5c77d54d61040caac09bcda954a8e9baa99c
Herik dos Santos Lorenção <heriksantoslorencao@gmail.com>
2022-03-08 09:25:54 -0300

    adicionando ler e gravar bytes em um fluxo de arquivo

--- Parte 8 - Codigo Antes/Parte 8/01 Sistema de Arquivos/Item_00.cs
+++ Parte 8 - Codigo Antes/Parte 8/01 Sistema de Arquivos/Item_00.cs
@@ -1,16 +1,38 @@
 ﻿using System;
+using System.IO;
 
 namespace Listings
 {
     class Item_00 //Streams
     {
-        static void Main(string[] args)
+        static void XMain(string[] args)
         {
             //TAREFAS:
             //1. ABRIR O ARQUIVO Diretores.txt
             //2. LER 10 BYTES DO ARQUIVO
             //3. IMPRIMIR ESSES BYTES NO CONSOLE
+            FileStream fileStream = new FileStream("Diretores.txt", FileMode.Open, FileAccess.Read);
 
+            byte[] array = new byte[10];
+            int posicao = 0;
+            int tamanho = 10;
+
+            fileStream.Read(array, posicao, tamanho);
+
+            foreach (var caracter in array)
+            {
+                Console.Write((char)caracter);
+            }
+
+            // fileStream.Seek(5, SeekOrigin.Current);
+            fileStream.Position = 3;
+            
+            fileStream.Read(array, posicao, tamanho);
+            
+            foreach (var caracter in array)
+            {
+                Console.Write((char)caracter);
+            }
         }
     }
 }
